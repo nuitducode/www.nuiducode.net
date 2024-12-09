23afd1231fc1cd9306c2d0437d09a914b91e3e25
laurent.abbal <laurent.abbal@aefe.fr>
2024-12-09 14:34:40 +0900

    ~

--- resources/views/24jdpp/24jdpp.blade.php
+++ resources/views/24jdpp/24jdpp.blade.php
@@ -3,6 +3,22 @@
 <head>
     @include('inc-meta')
     <title>24 jours de Python-Pyxel</title>
+    <style>
+        .del_button{
+            position: absolute;
+            z-index:1000;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            display: none;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+        }
+        .capture:hover .del_button {
+            display: block;
+        }
+    </style>
 </head>
 <body>
 
@@ -16,7 +32,7 @@
                 <div class="text-center"><img src="{{ url('/')}}/img/n-d-c.png" width="100" /></div>
                 <div class="text-center text-monospace text-dark mt-1 font-weight-bold" style="font-size:110%;">~ 24 JOURS DE PYTHON-PYXEL ~</div>	
                 <div class="text-center text-monospace text-danger mb-2 font-weight-bold" style="font-size:120%;">JOUR {{ $jour }}</div>	
-                <div class="text-center text-monospace mt-2 mb-2"><a  class="btn btn-light btn-sm" href="https://nuit-du-code.forge.apps.education.fr/24-jours-de-python-pyxel/" role="button" >voir tous les sujets</a></div>	
+                <div class="text-center text-monospace mt-2 mb-2"><a class="btn btn-light btn-sm" href="https://nuit-du-code.forge.apps.education.fr/24-jours-de-python-pyxel/" role="button" >voir tous les sujets</a></div>	
             </div>
         </div>
 
@@ -32,8 +48,15 @@
                         $capture = 'screenshot.png';
                     }
                     ?>
-                    @if($gif_exists OR $png_exists)
-                        <a href="/24jdpp-lecteur/{{ Crypt::encryptString($app['id']) }}"><img src="{{ asset('storage/vingtquatre-apps/jour-'.$app['jour'].'/'.$app['jeton'] .'/'.$capture) }}" width="200" style="margin:5px;border-radius:4px;"></a>
+                    @if(($gif_exists OR $png_exists) AND $app->is_public == 1)
+                        <div class="capture" style="position:relative;width:180px;margin:4px;display:inline-block">
+                            @if(Auth::check() && Auth::user()->is_admin == 1)
+                                <div class="del_button">
+                                    <a class="btn btn-danger btn-sm pl-3 pr-3" href="/24jdpp-capture-delete/{{ Crypt::encryptString($app['id']) }}" role="button"><i class="fa-solid fa-trash"></i></a>	
+                                </div>
+                            @endif
+                            <a href="/24jdpp-lecteur/{{ Crypt::encryptString($app['id']) }}"><img src="{{ asset('storage/vingtquatre-apps/jour-'.$app['jour'].'/'.$app['jeton'] .'/'.$capture) }}" width="100%" style="border-radius:4px;"></a>
+                        </div>
                     @endif
                 @endforeach
 		    </div>
